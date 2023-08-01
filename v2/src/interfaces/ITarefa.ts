e8b0e4c835f495fe072aa89083ed106e59b2fcdb
Juan Granke <juangranke@outlook.com>
2023-07-31 23:16:03 -0300

    novos codigos projeto v2

--- v2/src/interfaces/ITarefa.ts
+++ v2/src/interfaces/ITarefa.ts
@@ -1,4 +1,7 @@
+import IProjeto from "./IProjeto";
+
 export default interface ITarefa {
     duracaoEmSegundos: number,
-    descricao: string
+    descricao: string,
+    idProjeto: IProjeto
 }
--- v2/src/views/Projetos.vue
+++ v2/src/views/Projetos.vue
@@ -1,11 +1,21 @@
 <template>
-    <h1>Projetos</h1>
+    <div class="projetos">
+        <h1 class="title">Projetos</h1>
+        <router-view></router-view>
+    </div>
 </template>
 <script lang="ts">
 
-import { defineComponent } from 'vue'
+import { defineComponent } from 'vue';
 
 export default defineComponent({
     name: 'Projetos'
 })
 </script>
+<style scoped>
+
+    .projetos {
+        padding: 1.25rem;
+    }
+
+</style>
